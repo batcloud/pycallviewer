e785c03494ef00bedbf65b39ea8fea7bfc462cf5
Félix-Antoine Fortin <felix-antoine.fortin@calculquebec.ca>
2015-12-14 10:21:59 -0500

    Add key_event 'down' to summarize sound file

--- view_specgram.py
+++ view_specgram.py
@@ -2,6 +2,8 @@ import glob
 import sys
 import os
 
+import summary
+
 from functools import partial
 
 from scipy.io import wavfile
@@ -14,6 +16,7 @@ class SpecgramViewer(object):
         self.fig = plt.figure()
         self.fig.canvas.mpl_connect('key_press_event', partial(self.key_press))
         self.plot_current_call()
+        self.summarizer = summary.Summarizer()
         plt.show()
 
     def key_press(self, event):
@@ -23,6 +26,9 @@ class SpecgramViewer(object):
         elif event.key in ('p', 'left'):
             self.file_index = (self.file_index - 1) % len(self.filenames)
             self.plot_current_call()
+        elif event.key in ('down'):
+            print("".join((chr(27),"[2J",chr(27),"[H")))
+            print(self.summarizer.summarize(self.data, self.fs))
         elif event.key in ('q', 'escape'):
             plt.close()
 
@@ -30,16 +36,16 @@ class SpecgramViewer(object):
         self.fig.clear()
 
         filename = self.filenames[self.file_index]
-        fs, data = wavfile.read(filename)
-        if len(data.shape) == 1:
-            data = data.reshape(data.shape[0], 1)
+        self.fs, self.data = wavfile.read(filename)
+        if len(self.data.shape) == 1:
+            self.data = self.data.reshape(self.data.shape[0], 1)
 
-        for ch in range(data.shape[1]):
-            ax = self.fig.add_subplot(data.shape[1], 1, ch + 1)
+        for ch in range(self.data.shape[1]):
+            ax = self.fig.add_subplot(self.data.shape[1], 1, ch + 1)
             ax.set_title('%s, channel %i' % (filename, ch + 1))
             # TODO: edit spectrogram parameter
             # SEE: http://matplotlib.org/api/pyplot_api.html#matplotlib.pyplot.specgram
-            ax.specgram(data[:, ch], Fs=fs)
+            ax.specgram(self.data[:, ch], Fs=self.fs)
         self.fig.canvas.draw()
 
 if __name__ == "__main__":
